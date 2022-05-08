2118f1a253a190eda2ef9f532ee121eae4124678
Roger Zhang <1787009521@qq.com>
2022-05-08 19:55:30 +0800

    feat: app.use设置编码格式

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-    console.log(req.url)
-    res.write('hello')
+    res.set({ 'content-type': 'application/json; charset=utf-8' })
     next()
+})
+
+app.use((req, res, next) => {
+    if(req.url === '/' && req.method === 'GET') {
+        res.send('hello /')
+    } else {
+        res.write('hello') // 其中write是流的方式。
+        next()
+    }
     // res.end()
     // res.send()
 })
 
 app.use((req, res, next) => {
-    res.write(' world')
+    if(req.url === '/hello' && req.method === 'GET') {
+        res.write(' world') // 其中write是流的方式。
+        res.end()
+    } else {
+        next()
+    }
+})
+
+app.use((req, res, next) => {
+    res.write('404 错误页面!')
     res.end()
 })
+
 
 app.listen(3000, () => {
     console.log('正在监听3000端口')
